a896f3bf49a9573dd6ed4c0ee4b5411601b19a78
tuyishimejohnson <j.tuyishime4@alustudent.com>
2024-03-31 21:29:13 +0200

    Adding the contact page.

--- agura/src/components/Contacts.js
+++ agura/src/components/Contacts.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import FooterForm from './FooterForm';
+
 
 const Contacts = () => {
   return (
     <div>
-       < FooterForm />
+      <form action="">
+          <input type="text" name='name' id='name' placeholder='Name' />
+
+          <input type="email" name='email' id='email' placeholder='Email' />
+
+          <input type="text" name='subject' id='subject' placeholder='Subject'/>
+
+          <textarea name="message" id="message" cols="30" rows="10" placeholder='Message'></textarea>
+      </form>
     </div>
   )
 }
