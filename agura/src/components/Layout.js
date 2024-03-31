72aad9ae37109fc11bad44bb181a1cfe65198527
tuyishimejohnson <j.tuyishime4@alustudent.com>
2024-03-31 10:10:52 +0200

    Hamburger icon.

--- agura/src/components/Layout.js
+++ agura/src/components/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Layout = () => {
   return (
     <div>
-      <nav className='flex justify-between items-center py-6 px-32'>
+      <nav className='flex justify-between items-center py-6 px-32 sm:py-3 sm:px-16'>
         <h1 className='text-2xl text-blue-900 font-extrabold'>Agura Initiative</h1>
-        <ul className='flex gap-9'>
+        <ul className='flex gap-9 sm:hidden lg:flex'>
             <li className='navigation'>Home</li>
             <li className='navigation'>About us</li>
             <li className='navigation'>Services</li>
@@ -13,10 +15,13 @@ const Layout = () => {
         </ul>
 
         <div>
-            <ul className='flex gap-5'>
+            <ul className='flex gap-5 sm:hidden lg:flex'>
                 <li>Login</li>
                 <li>Sign Up</li>
             </ul>
+            <div className='hidden sm:flex lg:hidden text-4xl text-blue-900 items-center'>
+                < FontAwesomeIcon icon={faBars}/>
+            </div>
         </div>
       </nav>
     </div>
